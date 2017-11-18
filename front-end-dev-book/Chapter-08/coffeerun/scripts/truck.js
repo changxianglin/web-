c49fe677e0d0ac4221e5235a178a2d23f2d0777e
changxianglin <281532612@qq.com>
2017-11-18 13:39:49 +0800

    Chapter-08

---
+++ front-end-dev-book/Chapter-08/coffeerun/scripts/truck.js
@@ -0,0 +1,13 @@
+(function(window) {
+  'use strict'
+
+  var App = window.App || {}
+
+  function Truck(truckId, bd) {
+    this.truckId = truckId
+    this.db = db
+  }
+
+  App.Truck = Truck
+  window.App = App
+})(window)
