dccb7c92ad19f603da6ee33a3bf6f4cb4c5db2b7
changxianglin <281532612@qq.com>
2017-11-18 17:27:47 +0800

    chapter-08 coffee

--- front-end-dev-book/Chapter-08/coffeerun/scripts/truck.js
+++ front-end-dev-book/Chapter-08/coffeerun/scripts/truck.js
@@ -3,9 +3,27 @@
 
   var App = window.App || {}
 
-  function Truck(truckId, bd) {
+  function Truck(truckId, db) {
     this.truckId = truckId
     this.db = db
+  }
+
+  Truck.prototype.createOrder = function(order) {
+    console.log('Adding order of ' + order.emailAddress)
+    this.db.add(order.emailAddress, order)
+  }
+
+  Truck.prototype.deliveOrder = function(customerId) {
+    console.log('Delivering order for ' + customerId)
+    this.db.remove(customerId)
+  }
+
+  Truck.prototype.printOrders = function() {
+    var customerIdArray = Object.keys(this.db.getAll())
+    console.log('Truck # ' + this.truckId + ' has pending orders:')
+    customerIdArray.forEach(function (id) {
+      console.log(this.db.get(id))
+    }.bind(this))
   }
 
   App.Truck = Truck
